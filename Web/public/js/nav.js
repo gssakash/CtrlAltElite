5065704f1b99e75ac6c889c86b7c8ed44b423e41
Mohammad MuaZam <mm9084@srmist.edu.in>
2020-09-19 15:03:58 +0530

    added items

--- Web/public/js/nav.js
+++ Web/public/js/nav.js
@@ -1,39 +1,7 @@
-$(document).ready(function(){
-    // setTimeout(function(){
-    //   $('.navbar-brand').toggleClass('disappear');
-    //   }, 6000);
-    $(".navbar-toggler").click(function(){
-    
-      $('.navbar-toggler').toggleClass('shift');
-      // $('.navbar-brand').toggleClass('disappear');
-      
-    });
-  });
-  
-  $(".navbar-toggler").click(function () {
-    var el = $('.navbar-brand');
-    window.setTimeout(function() {
-        el.toggleClass('disappear');
-    }, 300);
-  });
-  
-  // $(document).click(function(e) {
-  // 	if (!$(e.target).is('.navbar-collapse')) {
-  //         $('.collapse').collapse('hide');
-  //         $(".navbar-toggler").addClass('shift');
-  
-  //         $(".navbar-brand").removeClass('disappear');
-  //         // $('.icon').toggleClass('newLocation');	    
-  //     }
-  // });
-  //  navbar bg changed after scrolling 50px
-  $(window).scroll(function() {
-      
-      if ($(document).scrollTop() > 50) {
-        $('.navbar').addClass('blurry');
-        $('.dropdown-menu').css("opacity",0.75);
-      } else {
-        $('.navbar').removeClass('blurry');
-        $('.dropdown-menu').css("opacity",0.85);
-      }
-    });
+$('.navbar-toggler').click(function(){
+  $('.navbar-toggler i').toggleClass("fa-times");
+});
+
+$(window).on('load',function(){
+  $('#loginModal').modal('show');
+});
